ebcb5e28f6d4b72a22cb179a046899f2727f05dc
Aditya Raj <raj.aditya3110@gmail.com>
2025-09-06 15:47:45 +0530

    Setting dimensions, visibility, location and Title for frame

--- src/bank/management/system/Login.java
+++ src/bank/management/system/Login.java
@@ -4,11 +4,25 @@ import javax.swing.*; //contains JFrame
 import java.awt.*; //contains Image class
 
 public class Login extends JFrame {
+        Login(){
 
+            //step2
+            setTitle("AUTOMATED TELLER MACHINE");
+            setLayout(null);//we will use our custom Layout
+
+            //step1
+            setSize(800,480);//set dimension of frame(length and breadth)
+            setVisible(true); //by default frame is not visible so we have to make it visible
+            setLocation(350,200);//by default when the frame starts it starts from top left, so to start from centre we used se location
+
+
+
+
+        }
 
 
 
     public static void main(String[] args) {
-
+            new Login();
     }
 }
