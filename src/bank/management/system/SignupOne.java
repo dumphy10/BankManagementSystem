aa29f95db086257a1c5e008aba6ea6e9695b0053
Aditya Raj <raj.aditya3110@gmail.com>
2025-09-07 02:21:39 +0530

    Used JLabel for writing "APPLICATION FORM NO" and using random class for generating 4 digit random no. for the form no.

--- src/bank/management/system/SignupOne.java
+++ src/bank/management/system/SignupOne.java
@@ -1,11 +1,36 @@
 package bank.management.system;
 
 import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
 
-public class SignupOne extends JFrame {
+public class SignupOne extends JFrame  {
 
     SignupOne(){
 
+        setLayout(null);
+
+
+        //generating random no. for form no.
+        Random ran = new Random();
+        long random = Math.abs((ran.nextLong() % 9000L) + 1000L); //To get only four digit positive no.
+
+        //Step3
+        //Form No
+        JLabel formno = new JLabel("APPLICATION FORM NO. " + random);//TO WRITE FORM NO. ON THE FRAME
+        formno.setFont(new Font("Raleway",Font.BOLD,38));
+        formno.setBounds(140,20,600,40);
+        add(formno);
+
+
+
+
+
+        //Step2
+        getContentPane().setBackground(Color.WHITE);
+
+
+        //Step1
         //setting frame
         setSize(850,800);
         setLocation(350,10);
