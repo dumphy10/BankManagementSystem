e17ceab147e422fb3ae62bf08fb219957086d192
Aditya Raj <raj.aditya3110@gmail.com>
2025-09-06 15:42:32 +0530

    Creating a Bank Management Project using Swing and awt to manage frames and images in aur Java application.

---
+++ src/bank/management/system/Login.java
@@ -0,0 +1,14 @@
+package bank.management.system;
+
+import javax.swing.*; //contains JFrame
+import java.awt.*; //contains Image class
+
+public class Login extends JFrame {
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
