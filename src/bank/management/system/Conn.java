1a9e06d14731c1760da90d2ba4b86f9e8d0b59d6
Aditya Raj <raj.aditya3110@gmail.com>
2025-09-08 23:40:35 +0530

    Setting JDBC to connect to database in Con(connection) class

---
+++ src/bank/management/system/Conn.java
@@ -0,0 +1,27 @@
+package bank.management.system;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.sql.*;
+
+public class Conn {
+
+    Connection c;
+    Statement s;
+    public Conn() {
+
+        //JDBC steps to connect to Database
+        try{
+            // Step1 to Register driver
+            Class.forName(com.mysql.cj.jdbc.Driver);
+            //Step2 create connection
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankmanagementsystem", "root", "12345");
+
+            //Step3 create statement
+            s = c.createStatement();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+}
