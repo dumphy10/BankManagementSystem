ba1600fd531b4d849dcaa10377c09d72457a0031
Aditya Raj <raj.aditya3110@gmail.com>
2025-09-11 14:35:09 +0530

    Now inserting text on atm image

--- src/bank/management/system/Transactions.java
+++ src/bank/management/system/Transactions.java
@@ -9,6 +9,7 @@ public class Transactions extends JFrame {
 
         setLayout(null);
 
+        //adding atm image
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/atm.jpg"));
         Image i2 = i1.getImage().getScaledInstance(900,900, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
@@ -16,9 +17,23 @@ public class Transactions extends JFrame {
         image.setBounds(0,0,900,900);
         add(image);
 
+        //adding text on atm image
+        JLabel text = new JLabel("Please select your Transaction");
+        text.setBounds(235,300,700,35);
+        text.setForeground(Color.WHITE);
+        text.setFont(new Font("System", Font.BOLD,16));
+        image.add(text);//setting text on the image
+
+
+
+
+
+
+
         setSize(900, 900);
         setLocation(300, 0);
-        setVisible(true);
+        setUndecorated(true);//remove the window decorations such as:  Title bar and Border around the frame
+        setVisible(true);//setvisible should be written in the end of these lines
 
     }
 
