a8a84acba0d6e31c5d7dc9e6b0c941d4709b2ac6
Aditya Raj <raj.aditya3110@gmail.com>
2025-09-11 14:49:23 +0530

    Now creating buttons on the image

--- src/bank/management/system/Transactions.java
+++ src/bank/management/system/Transactions.java
@@ -19,10 +19,39 @@ public class Transactions extends JFrame {
 
         //adding text on atm image
         JLabel text = new JLabel("Please select your Transaction");
-        text.setBounds(235,300,700,35);
+        text.setBounds(225,300,700,35);
         text.setForeground(Color.WHITE);
         text.setFont(new Font("System", Font.BOLD,16));
         image.add(text);//setting text on the image
+
+        //adding buttons on the image
+        JButton deposit = new JButton("Deposit");
+        deposit.setBounds(170,415,150,30);
+        image.add(deposit);
+
+        JButton withdrawl = new JButton("Cash Withdrawl");
+        withdrawl.setBounds(355,415,150,30);
+        image.add(withdrawl);
+
+        JButton fastCash = new JButton("Fast Cash");
+        fastCash.setBounds(170,450,150,30);
+        image.add(fastCash);
+
+        JButton miniStatement = new JButton("Mini Statement");
+        miniStatement.setBounds(355,450,150,30);
+        image.add(miniStatement);
+
+        JButton pinChange = new JButton("Pin Change");
+        pinChange.setBounds(170,485,150,30);
+        image.add(pinChange);
+
+        JButton balanceEnquiry = new JButton("Balance Enquiry");
+        balanceEnquiry.setBounds(355,485,150,30);
+        image.add(balanceEnquiry);
+
+        JButton exit = new JButton("Exit");
+        exit.setBounds(355,520,150,30);
+        image.add(exit);
 
 
 
@@ -32,7 +61,7 @@ public class Transactions extends JFrame {
 
         setSize(900, 900);
         setLocation(300, 0);
-        setUndecorated(true);//remove the window decorations such as:  Title bar and Border around the frame
+        //setUndecorated(true);//remove the window decorations such as:  Title bar and Border around the frame
         setVisible(true);//setvisible should be written in the end of these lines
 
     }
