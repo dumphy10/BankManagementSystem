2e7c742ebdbb560242555b79cfe0721bed7a0880
Aditya Raj <raj.aditya3110@gmail.com>
2025-09-11 14:18:07 +0530

    creating transaction page when we click the login button

---
+++ src/bank/management/system/Transactions.java
@@ -0,0 +1,27 @@
+package bank.management.system;
+
+import javax.swing.*;
+
+public class Transactions extends JFrame {
+
+    Transactions(){
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        new Transactions();
+    }
+}
+
