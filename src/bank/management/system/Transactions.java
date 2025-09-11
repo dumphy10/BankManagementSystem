47c30d06b99ca9d7ce00c6a4ba8d562856704fe5
Aditya Raj <raj.aditya3110@gmail.com>
2025-09-11 14:25:28 +0530

    inserting and setting atm image

--- src/bank/management/system/Transactions.java
+++ src/bank/management/system/Transactions.java
@@ -1,12 +1,20 @@
 package bank.management.system;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Transactions extends JFrame {
 
     Transactions(){
 
+        setLayout(null);
 
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/atm.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(900,900, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(0,0,900,900);
+        add(image);
 
         setSize(900, 900);
         setLocation(300, 0);
