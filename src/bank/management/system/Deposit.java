ce8fa5ef21ef92103908e4f019fc1c5d9e5c0381
Aditya Raj <raj.aditya3110@gmail.com>
2025-09-12 15:32:51 +0530

    Now we will create a deposit class for making logic , whenever a user want's to deposit money they can.

---
+++ src/bank/management/system/Deposit.java
@@ -0,0 +1,32 @@
+package bank.management.system;
+
+import javax.swing.*;
+
+public class Deposit extends JFrame {
+
+    Deposit(){
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        new Deposit();
+    }
+
+}
